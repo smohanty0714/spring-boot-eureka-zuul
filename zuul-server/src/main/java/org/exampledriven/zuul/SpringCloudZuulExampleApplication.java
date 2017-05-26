fa90c84982036853ce40ca43188c4d6b465f28e5
Suvendu Mohanty <sumohanty@ap.equinix.com>
2017-05-26 16:01:07 +0800

    Auto registering of services at Zuul

--- zuul-server/src/main/java/org/exampledriven/zuul/SpringCloudZuulExampleApplication.java
+++ zuul-server/src/main/java/org/exampledriven/zuul/SpringCloudZuulExampleApplication.java
@@ -2,6 +2,8 @@ package org.exampledriven.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,8 @@ import java.net.URL;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class SpringCloudZuulExampleApplication {
 
     public static void main(String[] args) throws MalformedURLException {
